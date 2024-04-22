a7554867b8346e0b7d5328f61ac3db35e2aadb0d
Romulo Felizola <felizolaromulo23@gmail.com>
2024-04-22 13:23:52 -0400

    paso los 2 test con exito de enviar formulario y el de mostrar lista de participantes, sigo con los siguientes pasos, calendario e impresion por pdf

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import SignaturePad from 'signature_pad';
-
-import Swal from 'sweetalert2'; // Importa SweetAlert
-
-document.addEventListener("livewire:load", function () {
-    let canvas = document.getElementById('signature-canvas');
-    let signaturePad = new SignaturePad(canvas);
-});
